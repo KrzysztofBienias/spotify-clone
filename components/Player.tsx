aa63aa8321091f36927afce4036b85cfa4a26524
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-07-28 21:34:22 +0200

    ✨ Created usePlayer hook with zustand store

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,6 +1,14 @@
 'use client';
 
+import usePlayer from '@/hooks/usePlayer';
+
 const Player = () => {
+    const player = usePlayer();
+
+    if (!player.activeId) {
+        return null;
+    }
+
     return <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">player</div>;
 };
 
