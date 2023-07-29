b9d18b47de8e3dfe644a0c08fc6cfa7d0d6fb9ee
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-07-29 12:18:59 +0200

    ✨ Created PlayerContent UI without functionalities

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,6 +3,7 @@
 import usePlayer from '@/hooks/usePlayer';
 import useGetSongById from '@/hooks/useGetSongById';
 import useLoadSongUrl from '@/hooks/useLoadSongUrl';
+import PlayerContent from '@/components/PlayerContent';
 
 const Player = () => {
     const player = usePlayer();
@@ -14,7 +15,11 @@ const Player = () => {
         return null;
     }
 
-    return <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">player</div>;
+    return (
+        <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+            <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
+        </div>
+    );
 };
 
 export default Player;
---
+++ components/PlayerContent.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { BsPauseFill, BsPlayFill } from 'react-icons/bs';
+import { AiFillStepBackward as BackwardIcon, AiFillStepForward as ForwardIcon } from 'react-icons/ai';
+import { HiSpeakerWave, HiSpeakerXMark } from 'react-icons/hi2';
+
+import { Song } from '@/types';
+import MediaItem from '@/components/MediaItem';
+import LikeButton from '@/components/LikeButton';
+
+interface Props {
+    song: Song;
+    songUrl: string;
+}
+
+const PlayerContent: React.FC<Props> = ({ song, songUrl }) => {
+    const PlayIcon = true ? BsPauseFill : BsPlayFill;
+    const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+
+    return (
+        <div className="grid grid-cols-2 md:grid-cols-3 h-full">
+            <div className="flex w-full justify-start">
+                <div className="flex items-center gap-x-4">
+                    <MediaItem song={song} />
+                    <LikeButton songId={song.id} />
+                </div>
+            </div>
+
+            {/* Mobile */}
+            <div className="flex md:hidden col-auto w-full justify-end items-center">
+                <div
+                    onClick={() => {}}
+                    className="w-10 h-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
+                >
+                    <PlayIcon size={30} className="text-black" />
+                </div>
+            </div>
+
+            {/* Desktop */}
+            <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
+                <BackwardIcon
+                    size={30}
+                    onClick={() => {}}
+                    className="text-neutral-400 cursor-pointer hover:text-white transition"
+                />
+
+                <div
+                    className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
+                    onClick={() => {}}
+                >
+                    <PlayIcon size={30} className="text-black" />
+                </div>
+
+                <ForwardIcon
+                    size={30}
+                    onClick={() => {}}
+                    className="text-neutral-400 cursor-pointer hover:text-white transition"
+                />
+            </div>
+
+            <div className="hidden md:flex w-dyll justify-end pr-2">
+                <div className="flex items-center gap-x-2 w-[120px]">
+                    <VolumeIcon size={34} onClick={() => {}} className="cursor-pointer" />
+
+                    {/*  Place for a slider...  */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PlayerContent;
