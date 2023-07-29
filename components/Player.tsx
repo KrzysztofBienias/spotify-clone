73b266c61b7c73152d55e17aba6f7acb540654c8
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-07-29 10:26:01 +0200

    ✨ Created useGetSongById and useLoadSongUrl hook

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,11 +1,16 @@
 'use client';
 
 import usePlayer from '@/hooks/usePlayer';
+import useGetSongById from '@/hooks/useGetSongById';
+import useLoadSongUrl from '@/hooks/useLoadSongUrl';
 
 const Player = () => {
     const player = usePlayer();
+    const { song } = useGetSongById(player.activeId);
 
-    if (!player.activeId) {
+    const songUrl = useLoadSongUrl(song!);
+
+    if (!song || !songUrl || !player.activeId) {
         return null;
     }
 
