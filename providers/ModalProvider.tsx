110d5f5fa249016dc8ac29a7e969291b39ca0341
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-08-10 22:36:33 +0200

    ✨ Created SubscribeModal and applied to ModalProvider

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -4,8 +4,14 @@ import { useEffect, useState } from 'react';
 
 import AuthModal from '@/components/AuthModal';
 import UploadModal from '@/components/UploadModal';
+import SubscribeModal from '@/components/SubscribeModal';
+import { ProductWithPrice } from '@/types';
 
-const ModalProvider = () => {
+interface Props {
+    products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<Props> = ({ products }) => {
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
@@ -20,6 +26,7 @@ const ModalProvider = () => {
         <>
             <AuthModal />
             <UploadModal />
+            <SubscribeModal products={products} />
         </>
     );
 };
